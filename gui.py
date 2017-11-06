21f1603b19f5339090f80cd3b7f613d1ae13972c
Tormec <dcstoyanov@gmail.com>
2017-11-06 09:12:03 +0100

    added entries for flow and head

--- gui.py
+++ gui.py
@@ -19,10 +19,34 @@ class Gui(tk.Frame):
         self.f_tab1.grid(row=0, column=0)
 
         self.f_tab2 = tk.Frame(n)
-        self.f_tab2.grid(row=0, column=0)
+        self.f_tab2.grid(row=0, column=1)
+
+        self.f_tab3 = tk.Frame(n)
+        self.f_tab3.grid(row=0, column=3)
 
         n.add(self.f_tab1, text="Feasability Study")
-        n.add(self.f_tab2, text="Pump Shaft Diameter")
+        n.add(self.f_tab2, text="Pump Shaft")
+        n.add(self.f_tab3, text="Impeller")
+
+        # entries for input variables
+        l_flow = tk.Label(self.f_tab1, text="Flow [m^3/s]")
+        l_flow.grid(row=0, column=0)
+        self.flow = tk.DoubleVar()
+        e_flow = tk.Entry(self.f_tab1, textvariable=self.flow, width=10)
+        e_flow.grid(row=0, column=1, padx=5)
+
+        l_head = tk.Label(self.f_tab1, text="Head [m]")
+        l_head.grid(row=0, column=2)
+        self.head = tk.DoubleVar()
+        e_head = tk.Entry(self.f_tab1, textvariable=self.head, width=10)
+        e_head.grid(row=0, column=3, padx=5)
+
+        b_calc = tk.Button(self.f_tab1, text="Calculate", command=self.calc)
+        b_calc.grid(row=0, column=4)
+
+    def calc(self):
+        print(self.flow.get())
+        print(self.head.get())
 
 
 def main():
