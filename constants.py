aa9df3d84f5a976379a0d00c5cc8152fa41d9c85
tormec <dcstoyanov@gmail.com>
2020-04-19 13:59:06 +0200

    added more internal diameters

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # physical constants
 G = 9.81  # gravity acceleration [m/s^2]
-RHO = 1000  # water density
+RHO = 1000  # volumic mass [kg/m^3]
 
 # polar pairs of an AC motor
 PPAIRS = [2, 4, 6, 8]
 
 # internal diameters for commercial pipes
-D_INT = [.054, .070, .082, .107, .132, .159, .207, .260, .310, .340]  # [m]
+D_INT = [.029, .037, .043, .054, .070, .082, .107, .132, .159, .207]  # [m]
