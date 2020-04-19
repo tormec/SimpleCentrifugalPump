cb3d20bc906e0efa50e2e20c0f4b5624567cdd11
tormec <dcstoyanov@gmail.com>
2020-04-19 19:33:15 +0200

    reference to ISO 4200

--- constants.py
+++ constants.py
@@ -5,5 +5,5 @@ RHO = 1000  # volumic mass [kg/m^3]
 # polar pairs of an AC motor
 PPAIRS = [2, 4, 6, 8]
 
-# internal diameters for commercial pipes
-D_INT = [.029, .037, .043, .054, .070, .082, .107, .132, .159, .207]  # [m]
+# internal diameters for commercial pipes UNI-ISO 4200, series 1, type D
+D_INT = [.029, .037, .043, .055, .070, .083, .107, .132, .160, .210]  # [m]
