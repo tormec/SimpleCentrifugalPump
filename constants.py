1a4569b086873c06b3235d0380eeedcf7a55b480
tormec <dcstoyanov@gmail.com>
2020-04-20 19:42:03 +0200

    rounded int diameters

--- constants.py
+++ constants.py
@@ -6,4 +6,4 @@ RHO = 1000  # volumic mass [kg/m^3]
 PPAIRS = [2, 4, 6, 8]
 
 # internal diameters for commercial pipes UNI-ISO 4200, series 1, type D
-D_INT = [.029, .037, .043, .055, .070, .083, .107, .132, .160, .210]  # [m]
+D_INT = [.030, .038, .044, .056, .071, .083, .108, .133, .160, .210]  # [m]
